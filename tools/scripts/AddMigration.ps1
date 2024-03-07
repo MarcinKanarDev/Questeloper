7c74998e8616267c8dfcc6d69a4ed0ab413b07e4
marcinkanardev <marcinkanardev@gmail.com>
2024-03-07 20:45:41 +0100

    Remove unecessary comments in script

--- tools/scripts/AddMigration.ps1
+++ tools/scripts/AddMigration.ps1
@@ -7,8 +7,6 @@ if ([string]::IsNullOrWhiteSpace($migrationName)) {
         # Set the location to the Infrastructure directory
         Set-Location -Path "../../server/src/Questeloper.Infrastructure"
 
-        # Now that you're in the Infrastructure directory, you can add the migration
-        # Assuming the startup project is relatively located as follows
         dotnet ef migrations add $migrationName -o ".\Persistence\Migrations" --startup-project "../Questeloper.Api"
 
         Write-Host "Migration '$migrationName' added successfully."
