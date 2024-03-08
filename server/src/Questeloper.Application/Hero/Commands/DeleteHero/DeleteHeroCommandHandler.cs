309a4c41b52efdc464d8cdb2a31a28df6051a369
Abachaczi <marcinkanardev.gmail.com>
2024-03-08 22:43:31 +0100

    Add delete hero command implementation

--- server/src/Questeloper.Application/Hero/Commands/DeleteHero/DeleteHeroCommandHandler.cs
+++ server/src/Questeloper.Application/Hero/Commands/DeleteHero/DeleteHeroCommandHandler.cs
@@ -4,8 +4,16 @@ namespace Questeloper.Application.Hero.Commands.DeleteHero;
 
 internal sealed class DeleteHeroCommandHandler : IRequestHandler<DeleteHeroCommand>
 {
-    public Task Handle(DeleteHeroCommand request, CancellationToken cancellationToken)
+
+    private readonly IHeroRepository _heroRepository;   
+        
+    public GetHeroQueryHandler(IHeroRepository heroRepository)
     {
-        throw new NotImplementedException();
+        _heroRepository = heroRepository;
+    }
+
+    public async Task Handle(DeleteHeroCommand request, CancellationToken cancellationToken)
+    {
+        await _heroRepository.DeleteHero(request.HeroId);
     }
 }
