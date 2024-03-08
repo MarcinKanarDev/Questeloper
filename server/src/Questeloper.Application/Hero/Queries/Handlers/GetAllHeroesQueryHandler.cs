595bfe4715deda5b33f3d621d1a0d5d4bd9e32ec
marcinkanardev <marcinkanardev.gmail.com>
2024-03-08 22:51:13 +0100

    Refactor GetAllHeroes command handler

--- server/src/Questeloper.Application/Hero/Queries/Handlers/GetAllHeroesQueryHandler.cs
+++ server/src/Questeloper.Application/Hero/Queries/Handlers/GetAllHeroesQueryHandler.cs
@@ -2,18 +2,12 @@
 
 namespace Questeloper.Application.Hero.Queries.Handlers;
 
-internal sealed class GetAllHeroesQueryHandler : IRequestHandler<GetAllHeroesQuery, IEnumerable<GetHeroResponse>>
+internal sealed class GetAllHeroesQueryHandler(IHeroRepository heroRepository) : IRequestHandler<GetAllHeroesQuery, IEnumerable<GetHeroResponse>>
 {
-    private readonly IHeroRepository _heroRepository;
-
-    public GetAllHeroesQueryHandler(IHeroRepository heroRepository)
-    {
-        _heroRepository = heroRepository;
-    }
 
     public async Task<IEnumerable<GetHeroResponse>> Handle(GetAllHeroesQuery request, CancellationToken cancellationToken)
     {
-        var result = await _heroRepository.GetAllHeroesAsync();
+        var result = await heroRepository.GetAllHeroesAsync();
 
         return result;
     }
