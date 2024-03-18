39cce4643b6719c937d757f4d46b9430731df622
marcinkanardev <marcinkanardev@gmail.com>
2024-03-18 22:03:47 +0100

    Add unit tests for CreateHeroCommandHandler and GetHeroQueryHandler

---
+++ server/tests/Questeloper.Tests.Unit/Hero/Commands/CreateHeroCommandHandlerTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Questeloper.Application.Abstractions;
+using Questeloper.Application.Hero.Commands.CreateHero;
+using Questeloper.Domain.Repositories;
+
+namespace Questeloper.Tests.Unit.Hero.Commands;
+
+public class CreateHeroCommandHandlerTests
+{
+    private readonly ICommandHandler<CreateHeroCommand, int> _commandHandler;
+    private readonly IHeroRepository _heroRepository;
+    
+    public CreateHeroCommandHandlerTests()
+    {
+        _heroRepository = Substitute.For<IHeroRepository>();
+        _commandHandler = new CreateHeroCommandHandler(_heroRepository);
+    }
+    
+    [Fact]
+    public async Task CreateHeroCommandHandler_ForValidCommand_ShouldCreateHeroAndInvokeHeroRepository()
+    {
+        //Arrange
+        const int expectedHeroId = 1; 
+        var command = new CreateHeroCommand("Hero", "Backend Developer");
+
+        _heroRepository
+            .CreateHeroAsync(Arg.Any<Domain.Entities.Hero>())
+            .Returns(Task.CompletedTask)
+            .AndDoes(ci => ci.Arg<Domain.Entities.Hero>().Id = expectedHeroId);
+        
+        //Act
+        var result = await _commandHandler.Handle(command, CancellationToken.None);
+        
+        //Assert
+        await _heroRepository
+            .Received(1)
+            .CreateHeroAsync(Arg.Is<Domain.Entities.Hero>(h =>
+                h.HeroName == command.Name && h.HeroClass.Name == command.HeroClass));
+        await _heroRepository.Received(1).CompleteAsync();
+        
+        result.Should().Be(expectedHeroId);
+    }
+}
---
+++ server/tests/Questeloper.Tests.Unit/Hero/Queries/GetHeroQueryHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using Questeloper.Application.Abstractions;
+using Questeloper.Application.Hero.Queries;
+using Questeloper.Application.Hero.Queries.Handlers;
+using Questeloper.Domain.Exceptions;
+using Questeloper.Domain.Repositories;
+using Questeloper.Domain.ValueObjects;
+
+namespace Questeloper.Tests.Unit.Hero.Queries;
+
+public class GetHeroQueryHandlerTests
+{
+    private readonly IQueryHandler<GetHeroQuery, GetHeroResponse> _queryHandler;
+    private readonly IHeroRepository _heroRepository;
+    
+    public GetHeroQueryHandlerTests()
+    {
+        _heroRepository = Substitute.For<IHeroRepository>();
+        _queryHandler = new GetHeroQueryHandler(_heroRepository);
+    }
+    
+    [Fact]
+    public async Task GetHeroQueryHandler_ForValidQuery_ShouldReturnCorrectGetHeroResponse()
+    {
+        //Arrange
+        var hero = new Domain.Entities.Hero("Hero", new HeroClass("Backend Developer"));
+        var query = new GetHeroQuery(hero.Id);
+        
+        _heroRepository.GetByIdAsync(Arg.Any<int>()).Returns(hero);
+        
+        //Act
+        var result = await _queryHandler.Handle(query, CancellationToken.None);
+        
+        //Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<GetHeroResponse>();
+        result.HeroClass.Should().Be(hero.HeroClass.Name);
+        result.Name.Should().Be(hero.HeroName.Value);
+        result.Level.Should().Be(hero.Level.LevelValue);
+        result.Experience.Should().Be(hero.Experience.ExperiencePoints);
+        result.HealthPoints.Should().Be(hero.HealthPoints.Points);
+        result.ManaPoints.Should().Be(hero.ManaPoints.Points);
+    }
+    
+    [Fact]
+    public async Task GetHeroQueryHandler_ForInvalidQuery_ShouldThrowHeroNotFoundException()
+    {
+        //Arrange
+        var query = new GetHeroQuery(1);
+        
+        _heroRepository.GetByIdAsync(Arg.Any<int>()).ReturnsNullForAnyArgs();
+        
+        //Assert
+        await Assert.ThrowsAsync<HeroNotFoundException>(() =>
+            _queryHandler.Handle(query, CancellationToken.None));
+    }
+}
