497d523137a866601a030a92287aa969a4d01413
marcinkanardev <marcinkanardev@gmail.com>
2024-03-20 16:58:25 +0100

    Add unit tests for Enemy query handlers

---
+++ server/tests/Questeloper.Tests.Unit/Enemy/Queries/GetAllEnemiesQueryHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Questeloper.Application.Abstractions;
+using Questeloper.Application.Enemy.Queries;
+using Questeloper.Application.Enemy.Queries.Handlers;
+using Questeloper.Domain.Repositories;
+
+namespace Questeloper.Tests.Unit.Enemy.Queries;
+
+public class GetAllEnemiesQueryHandlerTests
+{
+    private readonly IQueryHandler<GetAllEnemiesQuery, IEnumerable<GetEnemyResponse>> _queryHandler;
+    private readonly IEnemyRepository _enemyRepository;
+
+    public GetAllEnemiesQueryHandlerTests()
+    {
+        _enemyRepository = Substitute.For<IEnemyRepository>();
+        _queryHandler = new GetAllEnemiesQueryHandler(_enemyRepository);
+    }
+    
+    [Fact]
+    public async Task GetAllEnemiesQueryHandler_ForValidQuery_ShouldReturnCorrectGetEnemyResponse()
+    {
+        //Arrange
+        var enemies = new List<Domain.Entities.Enemy>
+        {
+            new("Enemy1"),
+            new("Enemy2")
+        };
+        var query = new GetAllEnemiesQuery();
+        
+        _enemyRepository.GetEnemiesAsync().Returns(enemies);
+        
+        //Act
+        var result = await _queryHandler.Handle(query, CancellationToken.None);
+        
+        //Assert
+        var getEnemyResponses = result.ToList();
+        
+        getEnemyResponses.Should().NotBeNull();
+        getEnemyResponses.Should().BeOfType<List<GetEnemyResponse>>();
+        getEnemyResponses.Count.Should().Be(enemies.Count);
+        getEnemyResponses.First().Name.Should().Be(enemies.First().Name.Value);
+        getEnemyResponses.First().Level.Should().Be(enemies.First().Level.LevelValue);
+        getEnemyResponses.First().HealthPoints.Should().Be(enemies.First().HealthPoints.Points);
+    }
+}
---
+++ server/tests/Questeloper.Tests.Unit/Enemy/Queries/GetEnemyByIdQueryHandlerTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using Questeloper.Application.Abstractions;
+using Questeloper.Application.Enemy.Queries;
+using Questeloper.Application.Enemy.Queries.Handlers;
+using Questeloper.Domain.Exceptions;
+using Questeloper.Domain.Repositories;
+
+namespace Questeloper.Tests.Unit.Enemy.Queries;
+
+public class GetEnemyByIdQueryHandlerTests
+{
+    private readonly IQueryHandler<GetEnemyByIdQuery, GetEnemyResponse> _queryHandler;
+    private readonly IEnemyRepository _enemyRepository;
+
+    public GetEnemyByIdQueryHandlerTests()
+    {
+        _enemyRepository = Substitute.For<IEnemyRepository>();
+        _queryHandler = new GetEnemyByIdQueryHandler(_enemyRepository);
+    }
+    
+    [Fact]
+    public async Task GetEnemyByIdQueryHandler_ForValidQuery_ShouldReturnCorrectGetEnemyResponse()
+    {
+        //Arrange
+        var enemy = new Domain.Entities.Enemy("Enemy");
+        var query = new GetEnemyByIdQuery(enemy.Id);
+        
+        _enemyRepository.GetByIdAsync(Arg.Any<int>()).Returns(enemy);
+        
+        //Act
+        var result = await _queryHandler.Handle(query, CancellationToken.None);
+        
+        //Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<GetEnemyResponse>();
+        result.Name.Should().Be(enemy.Name.Value);
+        result.Level.Should().Be(enemy.Level.LevelValue);
+        result.HealthPoints.Should().Be(enemy.HealthPoints.Points);
+    }
+    
+    [Fact]
+    public async Task GetEnemyByIdQueryHandler_ForInvalidQuery_ShouldThrowEnemyNotFoundException()
+    {
+        //Arrange
+        var query = new GetEnemyByIdQuery(1);
+        
+        _enemyRepository.GetByIdAsync(Arg.Any<int>()).ReturnsNullForAnyArgs();
+        
+        //Assert
+        await Assert.ThrowsAsync<EnemyNotFoundException>(() =>
+            _queryHandler.Handle(query, CancellationToken.None));
+    }
+}
