1b44cda3af217b497eca16347272a478bfb07822
marcinkanardev <marcinkanardev@gmail.com>
2024-04-03 21:52:11 +0200

    Initialize LoginPage component

---
+++ client/src/app/login-page/login-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css'
+})
+export class LoginPageComponent {
+
+}
